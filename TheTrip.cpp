59d78b3f0158190d08d41bf880af7487ac4d4228
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-17 18:46:28 -0700

    add TheTrip.cpp

---
+++ TheTrip.cpp
@@ -0,0 +1,14 @@
+
+/// -----------------
+/// Program Constants
+
+const int   MAX_STUDENTS = 1000     ;
+const float MAX_PRICE    = 10000.0f ;
+
+/// --------------
+/// Driver Program
+
+int main(int argc, char* argv[]) {
+
+
+}
