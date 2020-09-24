9e2fda2bc5a85e99b26ae3153a7587b59ed119b5
Carlos Cuenca <cuenca.cl@gmail.com>
2020-09-24 14:06:30 -0700

    rename Bank.cpp to 4367

---
+++ 4367.cpp
@@ -0,0 +1,27 @@
+/// ---------------------------------------------
+/// ICPC 4367 - Think I'll Buy Me a Football Team
+/// author - Carlos L. Cuenca
+/// Date - 09/24/2020
+
+#include <iostream>
+#include <vector>
+
+/// -----------------
+/// Program Constants
+
+const int EXIT_SUCCESS = 0;
+
+/// --------------
+/// Driver Program
+
+int main(int argc, char* argv[]) {
+
+
+	std::vector<std::vector<int>> matrix;
+
+
+
+	return EXIT_SUCCESS;
+
+}
+
