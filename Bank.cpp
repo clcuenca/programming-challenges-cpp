bedbfcc996eb890e3d45ffd0bbe9cc01d9760308
Carlos Cuenca <cuenca.cl@gmail.com>
2020-09-24 14:02:52 -0700

    document and add main.cpp

---
+++ Bank.cpp
@@ -0,0 +1,25 @@
+/// ---------------------------------------------
+/// ICPC 4367 - Think I'll Buy Me a Football Team
+/// author - Carlos L. Cuenca
+/// Date - 09/24/2020
+
+#include <iostream>
+
+/// -----------------
+/// Program Constants
+
+const int EXIT_SUCCESS = 0;
+
+/// --------------
+/// Driver Program
+
+int main(int argc, char* argv[]) {
+
+	
+
+
+
+	return EXIT_SUCCESS;
+
+}
+
