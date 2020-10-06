defe6f643e3402dba246a1bfbac9b51632ab69ad
Carlos Cuenca <cuenca.cl@gmail.com>
2020-10-06 03:15:47 -0700

    solve 7794 (Time Limit Exceeded)

--- 7794.cpp
+++ 7794.cpp
@@ -1,27 +1,17 @@
-/// ---------------------------
-/// ICPC 7794 - The Great Thief
-/// author - Carlos L. Cuenca
-/// Date - 09/24/2020
+/// ACM ICPC Live Archive
+/// 7794 The Great Thief
+/// Author: Carlos L. Cuenca
+/// Date: 10/05/20
 
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
 
-/// -------------
-/// Program Types
+/// -----------------
+/// Program Constants
 
-typedef uint64_t 			Index  	   ;
-typedef uint64_t 			Factor 	   ;
-typedef uint64_t			Count  	   ;
-typedef std::vector<Factor> FactorList ;
-
-/// -------------------
-/// Function Prototypes
-
-void setEvenFactors(FactorList&, Factor&);
-void setOddFactors(FactorList&, Factor&);
-void setPrimeFactors(FactorList&, Factor);
-Count getOmissionCount(FactorList&, Factor, Factor);
+const int SUCCESS = 0;
 
 /// --------------
 /// Driver Program
@@ -30,164 +20,101 @@ int main(int argc, char* argv[]) {
 
 	/// -----------------
 	/// Program Variables
+ 
+	uint64_t              maximum    ;
+	uint64_t              sum        ;
+	uint64_t  			  bitMaximum ;
 
-	Factor     maximum    ;
-	FactorList factorList ;
-	Factor     sum 		  ;
-
-	/// ---------
-	/// Main Loop
+	/// -------
+	/// Program
 
 	while(!std::cin.eof()) {
 
 		std::cin >> maximum;
 
-		sum = 0;		
+		sum = 0;
 
-		for(Factor current = 1; current <= maximum; current++) {
+		for(uint64_t current = 2; current <= maximum; current++) {
 
-			setPrimeFactors(factorList, current);
+			uint64_t buffer = current;
 
-			sum += (maximum - getOmissionCount(factorList, current, maximum));
+			std::vector<uint64_t> factorList ;
+
+			if(buffer != 2) {
+
+				if(!(buffer % 2)) factorList.push_back(2);
+
+				// We want to make sure the number is not divisible by 2
+				while(!(buffer % 2)) buffer >>= 1; 
+
+			}
+
+			for(uint64_t factor = 3; factor <= std::sqrt(buffer); factor += 2) {
+
+				// If there's a factor, add it to the list and reduce the number
+				if(!(buffer % factor) && buffer ^ factor) factorList.push_back(factor);
+
+				// Whittle the number down
+				while(!(buffer % factor)) buffer /= factor;
+
+			} 
+
+			// After Mangling the number, we make a final check to see if the prime number
+			// is greater than 2, not in the vector and not itself
+			if(buffer > 2 && current ^ buffer) factorList.push_back(buffer);
+
+			uint64_t omission = 0;
+
+			// This means the number is prime
+			if(!factorList.size()) {
+
+				omission = (maximum / current);
+
+			} else {
+
+				// Get the number of bits
+				bitMaximum = (1 << factorList.size()) - 1;
+
+				for(uint64_t index = 1; index <= bitMaximum; index++) {
+
+					uint64_t number  = index;
+					uint64_t jindex  = 0;
+					uint64_t factor  = 1;
+					uint64_t rolling = 0;
+
+					while(number > 0) {
+
+						if(number & 1) { 
+
+							factor *= factorList[jindex];
+							rolling++;
+
+						}
+
+						jindex++;
+
+						number >>= 1;
+
+					}
+
+					if(factor > 1) {
+					
+						if(rolling % 2) omission += (maximum / factor);
+						else 		  omission -= (maximum / factor);
+					}
+
+				}
+
+			}
+
+			sum += (maximum - omission);
 
 		}
 
-		std::cout << "Max: " << maximum << " Houses: " << maximum + 3 + sum << std::endl;
-
-
-	}
-
-
-	return EXIT_SUCCESS;
-
-}
-
-/// ------------------------
-/// Function Implementations
-
-/// -------------------------------------------
-/// @procedure: setEvenFactors
-/// @author: Carlos L Cuenca
-/// @parameter: List, Number
-/// @description: Sets the even factors of the
-/// number onto the given list
-
-void setEvenFactors(FactorList& factorList, Factor& number) {
-
-	if(!number || number == 2) return;
-
-	if(!(number % 2)) factorList.push_back(2);
-
-	// We want to make sure the number is no longer divisible by two
-	while(!(number % 2)) number >>= 1;
-
-}
-
-/// -------------------------------------------------
-/// @procedure: setOddFactors
-/// @author: Carlos L Cuenca
-/// @parameter: List, Number
-/// @description: Sets the odd factors of the number
-/// onto the given list, if any
-
-void setOddFactors(FactorList& factorList, Factor& number) {
-
-	// Skip an element since number is odd
-	for(Factor factor = 3; factor <= std::sqrt(number); factor += 2) {
-
-		// If there's a factor, add it to the list and reduce the number
-		if(!(number % factor) && number ^ factor) factorList.push_back(factor);
-
-		// Whittle the number down
-		while(!(number % factor)) number /= factor;
+		std::cout << maximum + sum + 3 << std::endl;
 
 	}
 
-}
-
-/// ------------------------------------------------------
-/// @procedure: SetFactors
-/// @author: Carlos L Cuenca
-/// @parameter: List, Factor
-/// @description: Sets all the prime factors of the given
-/// number into the vector.
-
-void setPrimeFactors(FactorList& factorList, Factor number) {
-
-	// Saving for a last-check
-	Factor buffer = number;
-
-	// Clear the list
-	factorList.clear();
-
-	// Get the 2's, if any
-	setEvenFactors(factorList, number);
-
-	// Get the odd Factors, if any
-	setOddFactors(factorList, number);
-
-	// After Mangling the number, we make a final check to see if the prime number
-	// is greater than 2, not in the vector and not itself
-	if(number > 2 && number ^ buffer) factorList.push_back(number);
-
-}
-
-Count getOmissionCount(FactorList& factorList, Factor number, Factor maximum) {
-
-   // This means the number is prime
-    if(!factorList.size()) return maximum/number;
-
-    Count  result       = 0;
-    Index  alternating  = 0;
-    Index  index        = 0;
-    Factor factor       = 0;
-
-    for(Index rolling_maximum = 0; rolling_maximum < factorList.size() ;){
-
-        factor = 1;
-
-        for(Index rolling = 0; rolling < rolling_maximum; rolling++) { 
-
-        	factor *= factorList[index + rolling];
-
-        	//std::cout << factorList[index + rolling] << "*";
-
-        }
-
-        if(alternating < factorList.size()) { 
-
-
-        	//std::cout << factorList[alternating] << std::endl;
-
-        	factor *= factorList[alternating++];
-
-        }
-
-        if(rolling_maximum % 2) result -= ((maximum/factor));
-        else                    result += ((maximum/factor));
-
-        if(alternating == factorList.size()){
-
-            if(rolling_maximum && (index + rolling_maximum < factorList.size() - 1)){
-
-                index++;
-
-                alternating = index + rolling_maximum;
-
-            }else{
-
-                rolling_maximum++;
-
-                alternating = rolling_maximum;
-
-                index = 0;
-
-            }
-
-        }
-
-    }
-
-    return result;
+	return SUCCESS;
 
 }
