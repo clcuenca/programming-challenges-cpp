151c9ad26c133cb78906f9fd12d8c017f03ce399
Carlos Cuenca <cuenca.cl@gmail.com>
2020-10-06 09:04:02 -0700

    add 7795

---
+++ 7795.cpp
@@ -0,0 +1,26 @@
+/// ACM ICPC Live Archive
+/// 7795 What's a cubit?
+/// Author: Carlos L. Cuenca
+/// Date: 10/05/20
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+
+/// -----------------
+/// Program Constants
+
+const int SUCCESS = 0;
+
+/// --------------
+/// Driver Program
+
+int main(int argc, char* argv[]) {
+
+
+
+
+	return SUCCESS;
+
+}
